988e9690e85698795e33708b92df15d24101a84a
numantariq435 <numantariq435@gmail.com>
2022-10-06 02:48:23 +0500

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import MessengerCustomerChat from 'react-messenger-customer-chat'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>BELMORABOT</p>
+
+        <MessengerCustomerChat
+          pageId='103856575835224'
+          appId='637122637933810'
+        />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
